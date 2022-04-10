4cda23e577f9502d4772035cb6dc44fed1724c74
jaino <msb3302@naver.com>
2022-04-10 15:25:33 +0900

    initial commit

---
+++ app/src/main/java/com/example/happysejong/ui/MyPageActivity.kt
@@ -0,0 +1,12 @@
+package com.example.happysejong.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.happysejong.R
+
+class MyPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_page)
+    }
+}
