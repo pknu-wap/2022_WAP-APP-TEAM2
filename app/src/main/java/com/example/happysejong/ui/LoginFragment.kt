bb601f284fbb333010280d0a37c22567bbd6c6cf
jaino <msb3302@naver.com>
2022-05-11 20:54:32 +0900

    create login function using firebase auth

--- app/src/main/java/com/example/happysejong/ui/LoginFragment.kt
+++ app/src/main/java/com/example/happysejong/ui/LoginFragment.kt
@@ -5,16 +5,59 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.happysejong.R
+import android.widget.Toast
+import androidx.navigation.NavDirections
+import androidx.core.widget.addTextChangedListener
+import androidx.navigation.fragment.findNavController
+import com.example.happysejong.databinding.FragmentLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginFragment : Fragment() {
+
+    private val binding by lazy{ FragmentLoginBinding.inflate(layoutInflater)}
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        initLoginButton()
+        initEmailAndPasswordEditText()
+        initSignUpTextView()
+        return binding.root
     }
+    private fun initLoginButton(){
+        binding.loginButton.setOnClickListener{
+            val email = binding.emailEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(requireActivity()){ task->
+                    if(task.isSuccessful){
+                        handleSuccessLogin()
+                    }else{
+                        Toast.makeText(activity, "입력 정보를 확인해주세요. ", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+    }
+    private fun initEmailAndPasswordEditText(){
+        binding.passwordEditText.addTextChangedListener{
+            val enable = binding.emailEditText.text.isNotEmpty() and binding.passwordEditText.text.isNotEmpty()
+            binding.loginButton.isEnabled = enable
+        }
+        binding.emailEditText.addTextChangedListener{
+            val enable = binding.emailEditText.text.isNotEmpty() and binding.passwordEditText.text.isNotEmpty()
+            binding.loginButton.isEnabled = enable
+        }
+    }
+    private fun handleSuccessLogin(){
 
+    }
+    private fun initSignUpTextView(){
+        binding.signUpTextView.setOnClickListener{
+            val directions: NavDirections = LoginFragmentDirections.actionLoginFragmentToSignUpFragment()
+            findNavController().navigate(directions)
+        }
+    }
 }
