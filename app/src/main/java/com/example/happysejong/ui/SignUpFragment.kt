dff83815deced5aba3e5bc305fa28d78730dd1d0
jaino <msb3302@naver.com>
2022-05-11 21:00:51 +0900

    create signUp function using firebase auth

--- app/src/main/java/com/example/happysejong/ui/SignUpFragment.kt
+++ app/src/main/java/com/example/happysejong/ui/SignUpFragment.kt
@@ -5,18 +5,52 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.happysejong.R
+import android.widget.Toast
+import androidx.navigation.NavDirections
+import androidx.navigation.fragment.findNavController
+import com.example.happysejong.databinding.FragmentSignUpBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 
 class SignUpFragment : Fragment() {
 
+    private val binding by lazy{ FragmentSignUpBinding.inflate(layoutInflater)}
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+        auth = Firebase.auth
+        initSignUpButton()
+        return binding.root
     }
+    private fun initSignUpButton(){
+        binding.signUpButton.setOnClickListener {
 
+            val email = binding.registerEmailEditText.text.toString()
+            val password = binding.registerPasswordEditText.text.toString()
+            val confirm = binding.registerConfirmEditText.text.toString()
+
+            if (password == confirm) {
+                auth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(requireActivity()) { task ->
+                        if (task.isSuccessful) {
+                            successSignUpHandle()
+                        } else {
+                            Toast.makeText(activity, "회원가입이 실패하였습니다.", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            }else{
+                Toast.makeText(activity, "비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+    private fun successSignUpHandle(){
+        Toast.makeText(activity, "회원가입 되었습니다.", Toast.LENGTH_SHORT).show()
+        val directions: NavDirections = SignUpFragmentDirections.actionSignUpFragmentToLoginFragment()
+        findNavController().navigate(directions)
+    }
 }
