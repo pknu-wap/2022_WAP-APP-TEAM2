6839e8ed19070e949a7dd220dc892df660416384
jaino <msb3302@naver.com>
2022-05-11 20:28:16 +0900

    create login screen

---
+++ app/src/main/java/com/example/happysejong/ui/LoginFragment.kt
@@ -0,0 +1,20 @@
+package com.example.happysejong.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.happysejong.R
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout  this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+}
