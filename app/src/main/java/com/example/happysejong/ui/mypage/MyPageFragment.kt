742281468f774e3dd055c3ec531a69c58aff4e43
jaino <msb3302@naver.com>
2022-05-12 20:24:18 +0900

    create signOut function in mypage

--- app/src/main/java/com/example/happysejong/ui/mypage/MyPageFragment.kt
+++ app/src/main/java/com/example/happysejong/ui/mypage/MyPageFragment.kt
@@ -1,5 +1,6 @@
 package com.example.happysejong.ui.mypage
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,16 +8,26 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.happysejong.R
 import com.example.happysejong.databinding.FragmentMyPageBinding
+import com.example.happysejong.ui.users.LoginActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class MyPageFragment : Fragment() {
 
     private val binding by lazy{ FragmentMyPageBinding.inflate(layoutInflater)}
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
 
+        auth = FirebaseAuth.getInstance()
+
+        binding.signOutButton.setOnClickListener {
+            auth.signOut()
+            val intent = Intent(activity, LoginActivity::class.java)
+            startActivity(intent)
+        }
         return binding.root
     }
 }
