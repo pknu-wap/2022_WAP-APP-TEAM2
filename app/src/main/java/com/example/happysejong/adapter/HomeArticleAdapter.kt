0693ecf456ebacaf97e210bd3805779198402e03
jaino <msb3302@naver.com>
2022-05-12 21:32:21 +0900

    create articleAdapter

--- app/src/main/java/com/example/happysejong/adapter/HomeArticleAdapter.kt
+++ app/src/main/java/com/example/happysejong/adapter/HomeArticleAdapter.kt
@@ -1,4 +1,51 @@
 package com.example.happysejong.adapter
 
-class HomeArticleAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.happysejong.databinding.ItemHomeMenuBinding
+import com.example.happysejong.model.ArticleModel
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+class ArticleAdapter(val onItemClicked: (ArticleModel) -> Unit) : ListAdapter<ArticleModel, ArticleAdapter.ViewHolder>(diffUtil) {
+
+    inner class ViewHolder(private val binding: ItemHomeMenuBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(articleModel: ArticleModel) {
+            val format = SimpleDateFormat("MM월 dd일")
+            val date = Date(articleModel.date)
+
+            binding.itemTitleView.text = articleModel.title
+            binding.itemDateTextView.text = format.format(date).toString()
+            binding.itemTextView.text = articleModel.content
+
+            binding.root.setOnClickListener {
+                onItemClicked(articleModel)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(ItemHomeMenuBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(currentList[position])
+    }
+
+    companion object {
+        val diffUtil = object : DiffUtil.ItemCallback<ArticleModel>() {
+            override fun areItemsTheSame(oldItem: ArticleModel, newItem: ArticleModel): Boolean {
+                return oldItem.date == newItem.date
+            }
+            override fun areContentsTheSame(oldItem: ArticleModel, newItem: ArticleModel): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+
 }
