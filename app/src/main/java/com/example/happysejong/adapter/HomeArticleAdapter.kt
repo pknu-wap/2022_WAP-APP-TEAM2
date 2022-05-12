1786494a7f38d62f6442de0c3c51de24c8a06923
jaino <msb3302@naver.com>
2022-05-12 17:26:11 +0900

    create adapter file, utils package

---
+++ app/src/main/java/com/example/happysejong/adapter/HomeArticleAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.happysejong.adapter
+
+class HomeArticleAdapter {
+}
---
+++ app/src/main/java/com/example/happysejong/utils/DBKeys.kt
@@ -0,0 +1,8 @@
+package com.example.happysejong.utils
+
+class DBKeys {
+    companion object{
+        const val DB_USERS = "Users"
+        const val DB_ARTICLES="Articles"
+    }
+}
