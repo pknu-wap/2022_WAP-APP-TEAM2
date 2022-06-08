60392de35b2157e3a4811314edf1bf3298aa5e56
jaino <msb3302@naver.com>
2022-06-08 21:46:57 +0900

    feat : application context file

---
+++ app/src/main/java/com/example/happysejong/App.kt
@@ -0,0 +1,15 @@
+package com.example.happysejong
+
+import android.app.Application
+import android.content.Context
+
+class App : Application() {
+    init {
+        instance = this
+    }
+    companion object {
+        private var instance: App? = null
+        fun applicationContext() : Context {
+            return instance!!.applicationContext
+        }
+    }
