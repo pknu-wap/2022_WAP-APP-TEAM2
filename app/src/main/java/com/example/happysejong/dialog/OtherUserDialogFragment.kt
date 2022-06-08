305233fd45babb29c86d69e7fe5cf92de9cdb9ee
jaino <msb3302@naver.com>
2022-06-08 17:45:37 +0900

    feat : 신고 기능 추가

--- app/src/main/java/com/example/happysejong/dialog/OtherUserDialogFragment.kt
+++ app/src/main/java/com/example/happysejong/dialog/OtherUserDialogFragment.kt
@@ -7,11 +7,28 @@ import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import androidx.navigation.fragment.navArgs
 import com.example.happysejong.databinding.DialogOtherUserFragmentBinding
+import com.example.happysejong.model.UserModel
 import com.example.happysejong.ui.chats.ChatsFragmentArgs
+import com.example.happysejong.utils.DBKeys
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class OtherUserDialogFragment : DialogFragment() {
 
     private val binding by lazy{ DialogOtherUserFragmentBinding.inflate(layoutInflater)}
+    private val userDB : DatabaseReference by lazy{
+        Firebase.database.reference.child(DBKeys.DB_USERS)}
+
+    override fun onStart() {
+        super.onStart()
+        val width = ViewGroup.LayoutParams.MATCH_PARENT
+        val height = ViewGroup.LayoutParams.WRAP_CONTENT
+        dialog?.window?.setLayout(width, height)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,17 +38,26 @@ class OtherUserDialogFragment : DialogFragment() {
         val args: OtherUserDialogFragmentArgs by navArgs()
         val userItem = args.userModel
 
-        binding.dialogOtherUserName.text = userItem.nickName
-        binding.dialogOtherUserDormitory.text = userItem.dormitory
+        userItemBinding(userItem)
+        initReportButton(userItem)
 
         return binding.root
     }
 
-    override fun onStart() {
-        super.onStart()
-        val width = ViewGroup.LayoutParams.MATCH_PARENT
-        val height = ViewGroup.LayoutParams.WRAP_CONTENT
-        dialog?.window?.setLayout(width, height)
-    }
+    private fun userItemBinding(userItem : UserModel){
 
+        binding.dialogOtherUserName.text = userItem.nickName
+        binding.dialogOtherUserDormitory.text = userItem.dormitory
+    }
+    private fun initReportButton(userItem: UserModel){
+        binding.reportButton.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch{
+                    userDB.child(userItem.uid).child("reports").get().addOnSuccessListener {
+                        val reports = it.value.toString().toLong() + 1
+                        userDB.child(userItem.uid).child("reports").setValue(reports)
+                        dismiss()
+                }
+            }
+        }
+    }
 }
