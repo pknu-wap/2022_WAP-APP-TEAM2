3ff159cf554f1aa6b4e16b56be96dd24775ab1a3
jaino <msb3302@naver.com>
2022-06-08 21:09:20 +0900

    feat : ChatModel update - Image, Toss

--- app/src/main/java/com/example/happysejong/model/ChatModel.kt
+++ app/src/main/java/com/example/happysejong/model/ChatModel.kt
@@ -3,6 +3,8 @@ package com.example.happysejong.model
 data class ChatModel (
     val users: UserModel,
     val message: String,
+    val isImage: Boolean,
+    val isToss: Boolean,
     val createdAt: Long
         ){
     constructor(): this(UserModel("","","",0),"", 0)
