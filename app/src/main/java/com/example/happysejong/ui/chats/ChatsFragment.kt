88231b2c7bcceceae377ebc3aad1e00f93edf32d
jaino <msb3302@naver.com>
2022-06-08 20:54:42 +0900

    feat : get image from gallery

--- app/src/main/java/com/example/happysejong/ui/chats/ChatsFragment.kt
+++ app/src/main/java/com/example/happysejong/ui/chats/ChatsFragment.kt
@@ -1,11 +1,18 @@
 package com.example.happysejong.ui.chats
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import androidx.core.content.PackageManagerCompat
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavDirections
@@ -23,6 +30,7 @@ import com.google.firebase.database.*
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import java.util.*
+import java.util.jar.Manifest
 
 class ChatsFragment : Fragment() {
 
@@ -57,14 +65,17 @@ class ChatsFragment : Fragment() {
             chatKey = it
             chatDB = Firebase.database.reference.child(DB_CHATS).child(chatKey)
             getChats()
-            Log.d("Tag", chatKey)
         }
+
         userDB.addValueEventListener(currentUserDBListener)
 
         initAddChatButton()
+        initGalleryButton()
+        initTossButton()
 
         return binding.root
     }
+
     private fun initAddChatButton(){
         binding.addChatButton.setOnClickListener{
             val message = binding.addChatsEditText.text.toString()
@@ -73,6 +84,7 @@ class ChatsFragment : Fragment() {
             binding.addChatsEditText.setText("")
         }
     }
+
     private fun getChats(){
         val chatList = mutableListOf<ChatModel>()
 
@@ -103,4 +115,46 @@ class ChatsFragment : Fragment() {
         binding.chatRecyclerView.adapter = adapter
         binding.chatRecyclerView.layoutManager = LinearLayoutManager(context)
     }
+
+    private fun initGalleryButton(){
+        binding.galleryButton.setOnClickListener{
+            when (PackageManager.PERMISSION_GRANTED) {
+                ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+                -> {
+                    selectImageFromGallery()
+                }
+                else -> {
+                    requestPermissionLauncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                }
+            }
+        }
+    }
+
+    private val selectImageFromGalleryResult =
+        registerForActivityResult(ActivityResultContracts.GetContent()) {
+            uri: Uri? -> uri?.let{
+
+        }
+    }
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {
+                selectImageFromGallery()
+            } else {
+                Toast.makeText(context, "Permissions not granted by the user.", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    private fun selectImageFromGallery() = selectImageFromGalleryResult.launch("image/*")
+
+    private fun initTossButton(){
+        binding.tossButton.setOnClickListener{
+
+        }
+    }
+
 }
