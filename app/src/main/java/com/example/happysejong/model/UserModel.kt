232a9efea1d272f6752c94fb0b6ebedf7755e50c
jaino <msb3302@naver.com>
2022-06-08 16:24:35 +0900

    feat : data class 2 parcelable

--- app/src/main/java/com/example/happysejong/model/UserModel.kt
+++ app/src/main/java/com/example/happysejong/model/UserModel.kt
@@ -1,10 +1,38 @@
 package com.example.happysejong.model
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class UserModel (
-    val nickName: String,
-    val dormitory: String,
-    val uid: String,
-    val reports: Long
-        ){
-    constructor(): this("", "", "", 0)
+    val nickName: String = "",
+    val dormitory: String = "",
+    val uid: String = "",
+    val reports: Long = 0
+        ) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString().toString(),
+        parcel.readString().toString(),
+        parcel.readString().toString(),
+        parcel.readLong()) {
+    }
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(nickName)
+        parcel.writeString(dormitory)
+        parcel.writeString(uid)
+        parcel.writeLong(reports)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<UserModel> {
+        override fun createFromParcel(parcel: Parcel): UserModel {
+            return UserModel(parcel)
+        }
+
+        override fun newArray(size: Int): Array<UserModel?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
