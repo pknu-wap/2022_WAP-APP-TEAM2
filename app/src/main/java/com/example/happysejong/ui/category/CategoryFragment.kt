6102fbab1b0ed1b39adf009010c7d4a816b998bc
Seunghyeok Lee <leesh7807@gmail.com>
2022-06-09 12:06:09 +0900

    add leesh2 commit

--- app/src/main/java/com/example/happysejong/ui/category/CategoryFragment.kt
+++ app/src/main/java/com/example/happysejong/ui/category/CategoryFragment.kt
@@ -5,15 +5,43 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.happysejong.R
+import com.example.happysejong.databinding.FragmentCategoryBinding
 
-class CategoryFragment : Fragment() {
+class CategoryFragment : Fragment(), View.OnClickListener{
+    private val binding by lazy{ FragmentCategoryBinding.inflate(layoutInflater)}
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
 
-        return inflater.inflate(R.layout.fragment_category, container, false)
+        initCategoryButton()
+        return binding.root
+    }
+
+    private fun initCategoryButton() {
+        binding.allButton.setOnClickListener(this)
+        binding.chickenButton.setOnClickListener(this)
+        binding.pizzaButton.setOnClickListener(this)
+        binding.koreaButton.setOnClickListener(this)
+        binding.japanButton.setOnClickListener(this)
+        binding.chinaButton.setOnClickListener(this)
+        binding.westButton.setOnClickListener(this)
+        binding.hamburgerButton.setOnClickListener(this)
+        binding.etcButton.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            binding.allButton.id -> {}
+            binding.chickenButton.id -> {}
+            binding.pizzaButton.id -> {}
+            binding.koreaButton.id -> {}
+            binding.japanButton.id -> {}
+            binding.chinaButton.id -> {}
+            binding.westButton.id -> {}
+            binding.hamburgerButton.id -> {}
+            binding.etcButton.id -> {}
+        }
     }
 }
--- app/src/main/java/com/example/happysejong/ui/home/AddArticleFragment.kt
+++ app/src/main/java/com/example/happysejong/ui/home/AddArticleFragment.kt
@@ -1,5 +1,7 @@
 package com.example.happysejong.ui.home
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -20,6 +22,7 @@ class AddArticleFragment : Fragment() {
 
     private val binding by lazy{ FragmentAddArticleBinding.inflate(layoutInflater)}
 
+
     private val articleDB : DatabaseReference by lazy {
         Firebase.database.reference.child(DB_ARTICLES).child(auth.currentUser!!.uid)
     }
@@ -30,18 +33,34 @@ class AddArticleFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
+
         auth = FirebaseAuth.getInstance()
+        initSelectCategoryButton()
         initAddArticleButton()
         return binding.root
     }
 
+    private fun initSelectCategoryButton(){ // 카테고리 AlertDialog
+        var foodArray: Array<String> = arrayOf("치킨", "피자", "한식", "일식", "중식", "양식", "패스트푸드", "기타")
+        binding.selectCategoryButton.setOnClickListener {
+            val builder = AlertDialog.Builder(activity)
+            builder.setTitle("카테고리 선택")
+                .setItems(foodArray,
+                    DialogInterface.OnClickListener{ dialog, which ->
+                        binding.selectCategoryButton.text = foodArray[which]
+                    })
+            builder.show()
+        }
+    }
     private fun initAddArticleButton(){
         binding.addArticleButton.setOnClickListener {
             val title = binding.articleTitleTextView.text.toString()
+            val category = binding.selectCategoryButton.text.toString()
+            val location = binding.articleLocationTextView.text.toString()
             val content = binding.articleTextTextView.text.toString()
 
-            if (title.isNotEmpty() && content.isNotEmpty()){
-                val model = ArticleModel(auth.uid.toString(), title, content, System.currentTimeMillis())
+            if (title.isNotEmpty() && category.isNotEmpty() && location.isNotEmpty() && content.isNotEmpty()){ // empty체크만 하고 DB로 올리는 코드는 쓰지 않음
+                val model = ArticleModel(auth.uid.toString(), title, content, category, location, System.currentTimeMillis())
                 articleDB.setValue(model)
                 val direction : NavDirections = AddArticleFragmentDirections.actionAddArticleFragmentToHomeFragment5()
                 findNavController().navigate(direction)
